3d586326ccbdfeaa526ac37c41bb05d0af9d92ba
«Hanna <Dandelion9825@gmail.com>
2020-11-02 13:15:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//TASK-1
+        // var a = Number(prompt("Введите начало диапазона", ""));
+        // var n = Number(prompt("Введите конец диапозона", ""));
+        // var sum = 0;
+        // while (a <= n) {
+        //     sum += a;
+        //     a++;
+        // }
+        // alert(sum)
+
+//TASK-4
+        // const num = prompt('Введите число', 0);
+        // if (isNaN(num)) {
+        // }
+        // else {
+        //     const num1 = num.length;
+        //     alert(`число содержит ${num1} цифры`);
+        // }
+
+// //TASK-7
+
+        // const num = prompt('Введите число',);
+        // const sliceNum = prompt('Насколько сдвинуть число?',);
+        // if (isNaN(num) || isNaN(sliceNum)) {
+        // }
+        // else {
+        //     alert(`Резултат - ${num.slice(sliceNum) + num.slice(0, sliceNum)}`);
+        // }
+
+//TASK-8
+        // const days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+        // let currDay = 0;
+        // while (confirm(`${days[currDay]}. Хотите увидеть следующий день?`)) {
+        //     currDay = (currDay + 1) % days.length;
+        // }
